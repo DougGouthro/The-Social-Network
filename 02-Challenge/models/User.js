861267fae066264cf6099a474b351f46cd667ae1
unknown <douggouthro@gmail.com>
2023-10-22 10:31:54 -0500

    user schema complete

---
+++ 02-Challenge/models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a user model
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique:true,
+      trimmed: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match:[/+.@+.\.+./,"Please enter valid email address"]
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref:'Thought'
+    }],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }],
+  
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function(){
+  return this.friends.lengh
+})
+
+const User = model('user', userSchema);
+
+module.exports = User;
--- 02-Challenge/models/index.js
+++ 02-Challenge/models/index.js
@@ -1,4 +1,4 @@
-const Student = require('./Student');
-const Course = require('./Course');
+const Student = require('./Thoughts');
+const Course = require('./User');
 
 module.exports = { Student, Course };
